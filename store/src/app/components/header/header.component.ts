93f208cdee3ceab1d2dbc71166bd719d48af09d0
trineesha thathsarani <trineeshasanduni125@gmail.com>
2023-07-30 01:00:50 +0530

    cart

--- store/src/app/components/header/header.component.ts
+++ store/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
-import { Cart } from 'src/app/models/cart.model';
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -24,7 +25,15 @@ export class HeaderComponent  {
     .reduce((prev,current)=>prev + current,0);
   }
 
-  constructor() { }
+  constructor(private cartService : CartService) { }
+  
+  getTotal(items: Array<CartItem>) : number{
+    return this.cartService.getTotal(items);
 
+  }
+
+  onClearCart(){
+    this.cartService.clearCart();
+  }
 
 }
--- store/src/app/pages/cart/cart.component.ts
+++ store/src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -33,17 +34,35 @@ export class CartComponent implements OnInit {
 
   ];
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.dataSource= this.cart.items;
+    this.cartService.cart.subscribe((_cart:Cart)=> {
+      this.cart= _cart;
+      this.dataSource = this.cart.items;
+    });
   }
 
   getTotal(items: Array<CartItem>) : number{
-    return items.
-      map((item) => item.price *item.quantity)
-      .reduce((prev,current) => prev + current, 0)
+    return this.cartService.getTotal(items);
 
   }
 
+  onClearCart():void{
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item:CartItem){
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item:CartItem):void{
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item:CartItem):void{
+    this.cartService.removeQuantity(item);
+  }
+
+  
 }
